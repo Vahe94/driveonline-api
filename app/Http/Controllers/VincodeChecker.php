db79fc4574aaa4ea87944da3c0fe2487786f134b
gartad_yan <gartad.yan@gmail.com>
2026-03-13 13:36:58 +0400

    post restore, edit, delete

--- app/Http/Controllers/VincodeChecker.php
+++ app/Http/Controllers/VincodeChecker.php
@@ -19,8 +19,9 @@ class VincodeChecker extends Controller
     private function makeRequest(string $vin): array
     {
         $api_key = config('services.gibdd_api_key');
+        $url = 'https://service.api-assist.com/parser/gibdd_api/accident?key=b1656d17713600b360f0457b06603290&vin=JN1FANF15U0109775';
         $api_url = 'https://service.api-assist.com/parser/gibdd_api/history?key='. $api_key .'&vin='. $vin;
-        $response = Http::get($api_url);
+        $response = Http::get($url);
         return $response->json();
     }
 }
