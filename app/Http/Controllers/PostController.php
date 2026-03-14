54615a6f297d6e40616da8ef4b35fba18b514777
gartad_yan <gartad.yan@gmail.com>
2026-03-14 15:27:05 +0400

    catalog filter

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,17 +8,60 @@ use App\Models\Post;
 use App\Enums\PostStatus;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Database\Query\Builder;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        return response()->json(
-            Post::ofStatus(PostStatus::APPROVED)->with('mainPhoto')->get()
-        );
+        $posts = Post::ofStatus(PostStatus::APPROVED)
+            ->when($request->has('title'), function (Builder $query, Request $request) {
+                $query->whereFullText('title', $request->title);
+            })
+            ->when($request->has('price'), function (Builder $query, Request $request) {
+                $query->whereBetween('price', [$request->price['min'], $request->price['max']]);
+            })
+            ->when($request->has('year'), function (Builder $query, Request $request) {
+                $query->whereBetween('year', [$request->year['min'], $request->year['max']]);
+            })
+            ->when($request->has('color'), function (Builder $query, Request $request) {
+                $query->where('color', $request->color);
+            })
+            ->when($request->has('wheel_position'), function (Builder $query, Request $request) {
+                $query->where('wheel_position', $request->wheel_position);
+            })
+            ->when($request->has('condition'), function (Builder $query, Request $request) {
+                $query->where('condition', $request->condition);
+            })
+            ->when($request->has('transmission'), function (Builder $query, Request $request) {
+                $query->where('transmission', $request->transmission);
+            })
+            ->when($request->has('drive_type'), function (Builder $query, Request $request) {
+                $query->where('drive_type', $request->drive_type);
+            })
+            ->when($request->has('body_type'), function (Builder $query, Request $request) {
+                $query->where('body_type', $request->body_type);
+            })
+            ->when($request->has('mileage'), function (Builder $query, Request $request) {
+                $query->whereBetween('mileage_amount', $request->mileage['min'], $request->mileage['max']);
+                $query->where('mileage_unit', $request->mileage['unit']);
+            })
+            ->when($request->has('engine_capacity'), function (Builder $query, Request $request) {
+                $query->whereBetween('engine_capacity', $request->engine_capacity['min'], $request->engine_capacity['max']);
+            })
+            ->when($request->has('fuel_type'), function (Builder $query, Request $request) {
+                $query->where('fuel_type', $request->fuel_type);
+            })
+            ->when($request->has('power'), function (Builder $query, Request $request) {
+                $query->whereBetween('power', $request->power['min'], $request->power['max']);
+            })
+        ->with('mainPhoto')
+        ->get();
+
+        return response()->json($posts);
     }
 
     /**
@@ -74,9 +117,7 @@ class PostController extends Controller
         $post = Post::withTrashed()->findOrFail($postId);
         $data = $request->all();
 
-        if (isset($data['details'])) {
-            $post->details()->update($data['details']);
-        }
+        $post->details()->update($data['details']);
 
         if (!$post->trashed()) {
             $data['status'] = PostStatus::WAITING;
--- database/migrations/2025_08_27_122518_create_posts_table.php
+++ database/migrations/2025_08_27_122518_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('title');
+            $table->string('title')->fulltext();
             $table->unsignedInteger('price');
             $table->string('status')->default(PostStatus::WAITING);
             $table->text('rejection_reason')->nullable();
